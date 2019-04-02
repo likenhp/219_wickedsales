f3ea5064afb999abd049a9329b87aac19aed238e
Kenneth Li <likenhp@gmail.com>
2019-04-02 16:57:54 -0700

    finished client routing -created a home page -created a products list page -created a products details page

---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+    <div className="home">
+        <h1 className="center purple-text text-darken-2">🧙‍♀️ Wicked Sales 🧙‍♀️</h1>
+    </div>
+)
