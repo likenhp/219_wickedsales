60d5cefc3a7b34dde9737136a10b56bfbc3d7371
Kenneth Li <likenhp@gmail.com>
2019-04-04 15:15:18 -0700

    completed mobile responsive nav and side nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,37 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import Sidenav from './sidenav'
 
 class Nav extends Component {
+    renderLinks(){
+        return (
+        <Fragment>
+            <li>
+                <Link to="/">Home</Link>
+            </li>,
+            <li>
+                <Link to="/products">Products</Link>
+            </li>
+        </Fragment>
+        )
+    }
+
     render(){
+        const links = this.renderLinks();
+
         return(
-            <div>
+            <Fragment>
                 <nav className="purple darken-4">
                 <div className="nav-wrapper">
                     <Link className="brand-logo" to="/">Wicked Sales</Link>
                     <a href="#" data-target="sidenav" className="sidenav-trigger">
                         <i className="material-icons">menu</i>
                     </a>
-
+                        
                     <ul className="right hide-on-med-and-down">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/products">Products</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sign In</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sign Up</Link>
-                        </li>
+                    {links}
                     </ul>
                 </div>
             </nav>
 
-            <Sidenav/>
-            </div>
+            <Sidenav links={links}/>
+            </Fragment>
         )
     }
 }
--- src/components/nav/sidenav.js
+++ src/components/nav/sidenav.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 
 class Sidenav extends Component {
     componentDidMount(){
-        console.log('sidenav mounted', this.sidenav)
         M.Sidenav.init(this.sidenav);
         
     }
@@ -11,19 +10,8 @@ class Sidenav extends Component {
     render(){
         return(
             <ul ref={(element)=>{this.sidenav = element}} id="sidenav" className="sidenav">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/products">Products</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sign In</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sign Up</Link>
-                        </li>
-                    </ul>
+                {this.props.links}
+            </ul>
         )
     }
 }
