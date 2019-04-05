92e444a9bf7b3b966d92e6e634dfab1da3712d11
Kenneth Li <likenhp@gmail.com>
2019-04-05 13:37:39 -0700

    completed endpoint creation

---
+++ public/api/addcartitem.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once('functions.php');
+
+set_exception_handler('handleError');
+
+require_once('config.php');
+
+require_once('mysqlconnect.php');
+
+$product_id = 1;
+//hardcode for now
+$product_quantity = 1;
+//setting this up for future functionality, for now hardcode a value
+//$cart_id = 1;
+//setting up for future, for now hardcoded
+$user_id = 1;
+//setting up for future, for now hardcoded
+
+$query = "SELECT `price` FROM `products` WHERE `id` = $product_id
+";
+
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+    throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_num_rows($result) === 0){
+    throw new Exception("no product matches product id $product_id");
+}
+
+$product_data = mysqli_fetch_assoc($result);
+
+$product_price = (int)$product_data['price'];
+
+$product_total = $product_price * $product_quantity;
+
+if(empty($cart_id)){ //empty checks if the variable exists and whether it is undefined, empty string, etc.
+    $cart_create_query = "INSERT INTO `carts` SET
+        `item_count` = $product_quantity, 
+        `total_price` = $product_total,
+        `created` = NOW(),
+        `users_id` = $user_id,
+        `changed` = NOW()
+        ";
+        print($cart_create_query);
+}
+
+//$query_cart = "SELECT `` FROM `carts`";
+
+?>
